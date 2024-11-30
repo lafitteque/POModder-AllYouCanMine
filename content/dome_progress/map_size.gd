f018ebf5795d6ff35a43ed70e1ba1b8412c92f20
lafitteque <quentin.laf@gmail.com>
2024-11-30 10:35:40 +0100

    Changement images progress

--- content/dome_progress/map_size.gd
+++ content/dome_progress/map_size.gd
@@ -5,16 +5,16 @@ var sprite_width = 18
 func set_map_size_to(map_size : String , col : int):
 	match map_size:
 		CONST.MAP_SMALL:
-			$small.texture = load("res://mods-unpacked/POModder-AllYouCanMine/images/progress_map" + str(col) + ".png")
+			$small.texture = load("res://mods-unpacked/POModder-AllYouCanMine/images/progress_map" + str(col+1) + ".png")
 			$small.self_modulate = Color(1.0,1.0,1.0,1.0)
 		CONST.MAP_MEDIUM:
-			$medium.texture = load("res://mods-unpacked/POModder-AllYouCanMine/images/progress_map" + str(col) + ".png")
+			$medium.texture = load("res://mods-unpacked/POModder-AllYouCanMine/images/progress_map" + str(col+1) + ".png")
 			$medium.self_modulate = Color(1.0,1.0,1.0,1.0)
 		CONST.MAP_LARGE:
-			$large.texture = load("res://mods-unpacked/POModder-AllYouCanMine/images/progress_map" + str(col) + ".png")
+			$large.texture = load("res://mods-unpacked/POModder-AllYouCanMine/images/progress_map" + str(col+1) + ".png")
 			$large.self_modulate = Color(1.0,1.0,1.0,1.0)
 		CONST.MAP_HUGE:
-			$huge.texture = load("res://mods-unpacked/POModder-AllYouCanMine/images/progress_map" + str(col) + ".png")
+			$huge.texture = load("res://mods-unpacked/POModder-AllYouCanMine/images/progress_map" + str(col+1) + ".png")
 			$huge.self_modulate = Color(1.0,1.0,1.0,1.0)
 			
 func set_children_custom_size(multiplyier : float):
